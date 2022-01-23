35e22ce79ce5c4d6b31977ae0598b686a70cea7b
Theo Dubiel <theodore.dubiel@gmail.com>
2022-01-22 19:19:02 -0600

    Added array of cool external tools made by frens.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,4 +1,4 @@
-import { Box, Card, Typography } from '@mui/material';
+import { Box, Card, Stack, Typography } from '@mui/material';
 import CardContent from '@mui/material/CardContent';
 import { useNavigate } from 'react-router-dom';
 
@@ -21,31 +21,88 @@ const tools = [
     }
 ] as Tool[]
 
+const externalTools = [
+    {
+        name: 'Extraterrestrial Finder',
+        link: 'https://finder.extraterrestrial.money/',
+        description: 'Blockchain explorer'
+    },
+    {
+        name: 'Coinhall',
+        link: 'https://coinhall.org/',
+        description: 'Live price charts'
+    },
+    {
+        name: 'Track Terra',
+        link: 'https://beta.trackterra.org/',
+        description: 'Transaction exporter for taxes'
+    },
+    {
+        name: 'Terra Dashboard',
+        link: 'https://terradashboard.com/',
+        description: 'Customizable dashboard for Terra'
+    }
+]
+
 export function Home() {
     const navigate = useNavigate();
 
+    const navigateToExternalLink = (link: string) => {
+        window.open(link, '_blank');
+    }
+
     return (
-        <Box
-            sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                flexWrap: 'wrap',
-                p: 1,
-                gap: '12px',
-            }}
-        >
-            {tools.map(tool => 
-                <Card key={tool.name} sx={{ maxWidth: 275, cursor: 'pointer', bgcolor: 'primary.light' }} onClick={() => navigate(tool.link)} >
-                    <CardContent>
-                        <Typography sx={{ fontSize: 24 }} gutterBottom>
-                            {tool.name}
-                        </Typography>
-                        <Typography variant="body2">
-                            {tool.description}
-                        </Typography>
-                    </CardContent>
-                </Card>
-            )}
-        </Box>
+        <Stack>
+            <Typography variant="h4" sx={{textAlign: 'center', marginTop: '24px'}}>
+                Supported Tools
+            </Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexWrap: 'wrap',
+                    p: 1,
+                    gap: '12px',
+                }}
+            >
+                {tools.map(tool => 
+                    <Card key={tool.name} sx={{ maxWidth: 275, cursor: 'pointer', bgcolor: 'primary.light' }} onClick={() => navigate(tool.link)} >
+                        <CardContent>
+                            <Typography sx={{ fontSize: 24 }} gutterBottom>
+                                {tool.name}
+                            </Typography>
+                            <Typography variant="body2">
+                                {tool.description}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                )}
+            </Box>
+            <Typography variant="h4" sx={{textAlign: 'center', marginTop: '24px'}}>
+                External Tools
+            </Typography>
+            <Box
+                sx={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    flexWrap: 'wrap',
+                    p: 1,
+                    gap: '12px',
+                }}
+            >
+                {externalTools.map(tool => 
+                    <Card key={tool.name} sx={{ maxWidth: 275, cursor: 'pointer', bgcolor: 'primary.light' }} onClick={() => navigateToExternalLink(tool.link)} >
+                        <CardContent>
+                            <Typography sx={{ fontSize: 24 }} gutterBottom>
+                                {tool.name}
+                            </Typography>
+                            <Typography variant="body2">
+                                {tool.description}
+                            </Typography>
+                        </CardContent>
+                    </Card>
+                )}
+            </Box>
+        </Stack>
     );
   }
