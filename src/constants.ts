01800e921d858fa8802e3657249b45b6fb036784
Brian Lee <brianlee1597@gmail.com>
2022-02-02 21:25:47 +0900

    visual denom name

--- src/constants.ts
+++ src/constants.ts
@@ -32,29 +32,10 @@ export const ANGEL_PROTO_ADDRESS_BOMBAY = "terra13au3ag9df7khs2sv7m485e5c5vfwwft
 export const visualDenomName = new Map([
     ["uusd", "UST"], ["uluna", "Luna"], ["usdr", "SDT"],
     ["ukrw", "KRT"], ["umnt", "MNT"], ["ueur", "EUT"],
-    ["ucny", "CNY"], ["uaud", "AUT"], ["ucad", "CAT"]
+    ["ucny", "CNY"], ["ujpy", "JPY"], ["ugbp", "GBP"],
+    ["uinr", "INR"], ["ucad", "CAT"], ["uchf", "CHF"],
+    ["uaud", "AUT"], ["usgd", "SGD"], ["uthb", "THB"],
+    ["usek", "SEK"], ["unok", "NOK"], ["udkk", "DKK"],
+    ["uidr", "IDR"], ["uphp", "PHP"], ["uhkd", "HKT"],
+    ["umyr", "MYR"], ["utwd", "TWD"]
 ])
-
-// "uluna": "0.01133",
-// "usdr": "0.104938",
-// "uusd": "0.15",
-// "ukrw": "170.0",
-// "umnt": "428.571",
-// "ueur": "0.125",
-// "ucny": "0.98",
-// "ujpy": "16.37",
-// "ugbp": "0.11",
-// "uinr": "10.88",
-// "ucad": "0.19",
-// "uchf": "0.14",
-// "uaud": "0.19",
-// "usgd": "0.2",
-// "uthb": "4.62",
-// "usek": "1.25",
-// "unok": "1.25",
-// "udkk": "0.9",
-// "uidr": "2180.0",
-// "uphp": "7.6",
-// "uhkd": "1.17",
-// "umyr": "0.6",
-// "utwd": "4.0"
